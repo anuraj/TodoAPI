5db72eea5df8a759b0459eddb6e18d10954f0fa1
Anuraj <anuraj.p@msn.com>
2020-11-10 19:10:46 +0530

    Tags model support added.

---
+++ src/Migrations/20201110134014_TagsAdded.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoApi.Migrations
+{
+    public partial class TagsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TodoItems",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    TodoId = table.Column<int>(nullable: false),
+                    TodoItemId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tag_TodoItems_TodoItemId",
+                        column: x => x.TodoItemId,
+                        principalTable: "TodoItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tag_TodoItemId",
+                table: "Tag",
+                column: "TodoItemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TodoItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- src/Models/TodoItem.cs
+++ src/Models/TodoItem.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TodoApi.Models
 {
     public class TodoItem
     {
         public int Id { get; set; }
+        [Required]
         public string Description { get; set; }
         public bool IsCompleted { get; set; } = false;
-        public DateTime CreatedOn { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public List<Tag> Tags { get; set; }
     }
 }
